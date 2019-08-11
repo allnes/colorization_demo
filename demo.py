d5e5f4a3234aab7bbd2f0ac77f11fecdf4f6737e
Alexander Nesterov <nesoldr@mail.ru>
2019-08-11 20:22:20 +0000

    update demo

--- demo.py
+++ demo.py
@@ -2,12 +2,16 @@ from openvino.inference_engine import IENetwork, IECore
 import cv2 as cv
 import numpy as np
 
-net = IENetwork(model="colorization.xml", weights="colorization.bin")
-ie = IECore()
-exec_net = ie.load_network(network=net, device_name="CPU")
+scale = 255
+mean  = 50
 
-input_blob = next(iter(net.inputs))
-n, c, h_in, w_in = net.inputs[input_blob].shape
+load_net = IENetwork(model="colorization.xml", weights="colorization.bin")
+exec_net = IECore().load_network(network=load_net, device_name="CPU")
+
+input_blob  = next(iter(load_net.inputs))
+output_blob = next(iter(load_net.outputs))
+
+n, c, h_in, w_in = load_net.inputs[input_blob].shape
 images = np.ndarray(shape=(n, c, h_in, w_in))
 
 if 1:
@@ -16,22 +20,22 @@ else:
     cap = cv.VideoCapture(0)
 
 while cv.waitKey(1) < 0:
-    hasFrame, original_frame = cap.read()
-    if not hasFrame:
+    has_frame, original_frame = cap.read()
+    (h_orig, w_orig) = original_frame.shape[:2]
+    if not has_frame:
         cv.waitKey()
         break
 
     # preprocessing frame
     frame = cv.cvtColor(cv.cvtColor(original_frame, cv.COLOR_RGB2GRAY), cv.COLOR_GRAY2RGB)
-    img_rgb = (frame[:,:,[2, 1, 0]] * 1.0 / 255).astype(np.float32)
+    img_rgb = (frame[:,:,[2, 1, 0]] * 1.0 / scale).astype(np.float32)
     img_lab = cv.cvtColor(img_rgb, cv.COLOR_RGB2Lab)
-    img_l = img_lab[:,:,0] # pull out L channel
-    (H_orig,W_orig) = img_rgb.shape[:2] # original image size
+    img_l = img_lab[:,:,0]
 
     img_rs = cv.resize(img_rgb, (w_in, h_in))
     img_lab_rs = cv.cvtColor(img_rs, cv.COLOR_RGB2Lab)
     img_l_rs = img_lab_rs[:,:,0]
-    img_l_rs -= 50
+    img_l_rs -= mean
 
     for i in range(n):
         images[i] = img_l_rs
@@ -40,15 +44,16 @@ while cv.waitKey(1) < 0:
     res = exec_net.infer(inputs={input_blob: images})
 
     # get result
-    out = res['class8_ab'][0,:,:,:].transpose((1,2,0))
-    out = cv.resize(out, (W_orig, H_orig))
+    out = res[output_blob][0,:,:,:].transpose((1,2,0))
+    out = cv.resize(out, (w_orig, h_orig))
     img_lab_out = np.concatenate((img_l[:,:,np.newaxis],out),axis=2)
     img_bgr_out = np.clip(cv.cvtColor(img_lab_out, cv.COLOR_Lab2BGR), 0, 1)
 
     # show result
-    imshowSize = (640, 480)
-    frame = cv.resize(frame, imshowSize)
-    original_frame = cv.resize(original_frame, imshowSize)
+    show_size = (640, 480)
+    frame = cv.resize(frame, show_size)
+    original_frame = cv.resize(original_frame, show_size)
+    colorized_frame = cv.resize(img_bgr_out, show_size)
     cv.imshow('origin', original_frame)
-    cv.imshow('gray', cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
-    cv.imshow('colorized', cv.resize(img_bgr_out, imshowSize))
+    cv.imshow('gray', frame)
+    cv.imshow('colorized', colorized_frame)
